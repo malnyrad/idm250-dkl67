b9023253ea5566072878ae98c0ad0d394f6f5217
Daryn Lam <dkl67@drexel.edu>
2025-03-16 00:32:06 -0400

    Update header

--- header.php
+++ header.php
@@ -1,28 +1,18 @@
 <!DOCTYPE html>
-<!--home page-->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LAMDA | home</title>
+    <title>
+        <?php
+            wp_title('|', true, 'right'); // About
+            bloginfo('name'); // IDM250
+        ?>
+    </title>
     <!--<link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">-->
+    <?php wp_head(); ?>
 </head>
-<body class="bg-deco">
-    <header class="header-bar">
-        <h6><a class='logo' href='/' id='top'>&lambda;</a></h6>
-        <!--navigation-->
-        <nav>
-            <input id="toggle" type="checkbox">
-            <label class="menu-button" for="toggle">
-                <img src="images/hamburger-menu.svg" class="light-filter" alt="Hamburger menu">
-                <span></span>
-            </label>
-            <ul class="bg-deco-2 menu-overlay">
-                <li><a class='lower-case nutmeg-inv-text selected-menu-item' href='/'>Home</a></li>
-                <li><a class='lower-case menu-item' href='/personal'>Personal</a></li>
-                <li><a class='lower-case menu-item' href='/about'>About</a></li>
-                <li><a class='lower-case menu-item' href='/resume'>Resume</a></li>
-            </ul>
-        </nav>
-    </header>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <?php get_template_part('components/navigation'); ?>
