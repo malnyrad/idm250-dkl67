024ec9d01c335396fc0b7db8f597b524ba58cd49
Daryn Lam <dkl67@drexel.edu>
2025-03-17 11:05:33 -0400

    Update 404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,18 @@
-error, page not found
+    <?php get_header(); ?>
+    <div class="wrapper">
+        <div class="error-404">
+            <h3>404: Page Not Found</h3>
+            <p>Sorry, the page you're looking for doesn't exist.</p>
+            <!-- Add some helpful navigation 
+            <nav class="error-navigation">
+                <?php
+                        wp_nav_menu([
+                            'theme_location' => '404-menu',
+                            'menu_class' => 'error-menu',
+                            'container' => false
+                        ]);
+                ?>
+            </nav>-->
+        </div>
+    </div>
+    <?php get_footer(); ?>
