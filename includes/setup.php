66cd8c2dab4156cd572575990a5e6de84d331464
Daryn Lam <dkl67@drexel.edu>
2025-03-17 12:41:16 -0400

    Update header and css

--- includes/setup.php
+++ includes/setup.php
@@ -1,9 +1,28 @@
 <?php
     function theme_scripts_and_styles() {
         wp_enqueue_style(
-            'idm-normalize',
+            'normalize-css',
             'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
             [],
+        );
+
+        wp_enqueue_style(
+            'bootstrap-css',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+        );
+
+        wp_enqueue_style(
+            'idm-variables',
+            get_template_directory_uri() . '/dist/css/variables.css',
+            [],
+            filemtime(get_template_directory() . '/dist/css/variables.css')
+        );
+
+        wp_enqueue_style(
+            'idm-navigation',
+            get_template_directory_uri() . '/dist/css/navigation.css',
+            [],
+            filemtime(get_template_directory() . '/dist/css/navigation.css')
         );
 
         wp_enqueue_style(
--- page.php
+++ page.php
@@ -1,15 +1,36 @@
 <?php get_header(); ?>
-<div class="wrapper">
-    <h1 class="page-header">
-        <?php echo get_the_title(); ?>
-    </h1>
+<?php
+    $title_full = strtoupper(get_the_title());
+    $title_parts = explode(':', $title_full);
 
-    <?php if (has_post_thumbnail()) : ?>
-    <div class="featured-image">
-        <?php the_post_thumbnail(); ?>
+    $title = trim($title_parts[0]);
+    $snippit = isset($title_parts[1]) ? trim($title_parts[1]) : '';
+?>
+
+<div class="wp-block-columns">
+    <div class="wp-block-column is-vertically-aligned-center">
+        <h1 class="down text-color-orange"><?php echo $title; ?></h1>
+        <?php if (!empty($snippit)): ?>
+            <h2 class="text-color-blue"><?php echo $snippit; ?></h2>
+        <?php endif; ?>
+
+        <div class="wp-block-buttons">
+            <div class="wp-block-button">
+                <a class="wp-block-button__link wp-element-button" href="<?php echo esc_url(home_url('/')); ?>">See My Work</a>
+            </div>
+        </div>
     </div>
-    <?php endif; ?>
+    <?php if (has_post_thumbnail()) : ?>
+    <div class="wp-block-column">
+        <div class="wp-block-cover aligncenter has-custom-content-position is-position-top-left">
+            <?php $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
+            <img class="wp-block-cover__image-background wp-image-107" alt="" src="<?php echo esc_url($thumbnail_url); ?>">
+        </div>
+    </div>
+    <?php endif; ?> 
+</div>
 
-    <?php get_template_part('components/content'); ?>
+<div class="main-content">
+    <?php echo get_the_content(); ?>
 </div>
 <?php get_footer(); ?>
