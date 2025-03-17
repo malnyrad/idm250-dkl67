06192a03cc4eccb44c081394d8e21e1532b843c9
Daryn Lam <dkl67@drexel.edu>
2025-03-17 18:58:44 -0400

    Update page.php

--- page.php
+++ page.php
@@ -1,11 +1,11 @@
-    <?php get_header(); ?>
-    <main>
-        <h2 class="page-header center-align"><?php echo strtolower(get_the_title()); ?></h2>
-        <?php if (has_post_thumbnail()) : ?>
-        <div class="featured-image">
-            <?php the_post_thumbnail(); ?>
-        </div>
-        <?php endif; ?>
-        <?php get_template_part("components/content"); ?>
-    </main>
-    <?php get_footer(); ?>
+<?php get_header(); ?>
+<main>
+    <h2 class="page-header center-align"><?php echo strtolower(get_the_title()); ?></h2>
+    <?php if (has_post_thumbnail()) : ?>
+    <div class="featured-image">
+        <?php the_post_thumbnail(); ?>
+    </div>
+    <?php endif; ?>
+    <?php get_template_part("components/content"); ?>
+</main>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,11 +1,10 @@
 <?php get_header(); ?>
-
 <main class="post-container">
     This is the <strong>single.php</strong> page.
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article class="post">
             <h1 class="post-title"><?php the_title(); ?></h1>
-            <p class="post-meta">Published on <?php the_date(); ?> in <?php the_category(', '); ?></p>
+            <p class="post-meta">Published on <?php the_date(); ?> in <?php the_category(", "); ?></p>
             
             <?php if (has_post_thumbnail()) : ?>
                 <div class="post-thumbnail">
@@ -18,10 +17,9 @@
             </div>
             
             <div class="post-tags">
-                <?php the_tags('Tags: ', ', '); ?>
+                <?php the_tags("Tags: ", ", "); ?>
             </div>
         </article>
     <?php endwhile; endif; ?>
 </main>
-
 <?php get_footer(); ?>
