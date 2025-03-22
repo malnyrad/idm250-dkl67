042a453cd5a856b3999b4312c8be4166cf054345
Daryn Lam <dkl67@drexel.edu>
2025-03-22 19:09:12 -0400

    Update page.php

--- front-page.php
+++ front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <?php get_template_part("components/home-title"); ?>
 <?php get_template_part("components/latest-projects"); ?>
+</div>
 </main>
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,11 +1,14 @@
 <?php get_header(); ?>
-<main>
-    <h2 class="page-header center-align"><?php echo get_the_title(); ?></h2>
-    <?php if (has_post_thumbnail()) : ?>
-    <div class="featured-image">
-        <?php the_post_thumbnail(); ?>
+<main class="bg-deco">
+    <div class="wrapper">
+        <h2 class="page-header center-align"><?php echo get_the_title(); ?></h2>
+        <?php if (has_post_thumbnail()) : ?>
+        <div class="page-image">
+            <?php the_post_thumbnail(); ?>
+            <p><?php echo get_the_excerpt(); ?></p>
+        </div>
+        <?php endif; ?>
+        <?php get_template_part("components/content"); ?>
     </div>
-    <?php endif; ?>
-    <?php get_template_part("components/content"); ?>
 </main>
 <?php get_footer(); ?>
