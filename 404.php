395681f79250f1351900051a5e1e348953779d4d
Daryn Lam <dkl67@drexel.edu>
2025-03-23 01:20:39 -0400

    Update 404 page

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
-    <?php get_header(); ?>
-    <div class="wrapper">
-        <div class="error-404">
-            <h3>404: Page Not Found</h3>
-            <p>Sorry, the page you're looking for doesn't exist.</p>
-        </div>
+<?php get_header(); ?>
+<main>
+<div class="wrapper">
+    <div class="error-404">
+        <h3>404: Page Not Found</h3>
+        <p>Sorry, the page you're looking for doesn't exist.</p>
     </div>
-    <?php get_footer(); ?>
+</div>
+</main>
+<?php get_footer(); ?>
