5076034dc156219f9725018e7ac160dc7dfbb956
Daryn Lam <dkl67@drexel.edu>
2025-03-23 01:32:38 -0400

    Update archive page

--- archive.php
+++ archive.php
@@ -1,21 +1,26 @@
 <?php get_header(); ?>
-<div class="wrapper">
-  <div class="archive-header">
-    <h1><?php the_archive_title(); ?></h1>
-    <?php the_archive_description(); ?>
-  </div>
+<main class="bg-deco">
+  <div class="wrapper">
+    <h2 class="page-header center-align">Projects</h2>
+    <?php
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $args = [
+            'post_type' => 'projects',
+            'posts_per_page' => 10,
+            'paged' => $paged,
+        ];
+        $query = new WP_Query($args);
 
-  <?php if (have_posts()) : ?>
-  <h1><?php the_archive_title(); ?></h1>
-
-    <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part("components/project-card"); ?>
-    <?php endwhile; ?>
-
-  <?php the_posts_pagination(); ?>
-
-  <?php else : ?>
-  <p>No posts found.</p>
-  <?php endif; ?>
-</div>
+        if ($query->have_posts()) :
+          while ($query->have_posts()) : $query->the_post();
+          // for each post, render this component
+          get_template_part("components/project-card");
+          endwhile;
+        get_template_part('components/pagination.php');
+        wp_reset_postdata();
+        else : ?>
+      <p><?php esc_html_e('No projects found.', 'idm250-2025'); ?></p>
+      <?php endif; ?>
+    </div>
+</main>
 <?php get_footer(); ?>
--- templates/project-listing.php
+++ templates/project-listing.php
@@ -4,31 +4,28 @@
  */
 
 get_header(); ?>
+<main class="bg-deco">
+  <div class="wrapper">
+    <h2 class="page-header center-align">Projects</h2>
+    <?php
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $args = [
+            'post_type' => 'projects',
+            'posts_per_page' => 10,
+            'paged' => $paged,
+        ];
+        $query = new WP_Query($args);
 
-<div class="project-listing">
-  <h1>Projects</h1>
-  <?php
-  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$args = [
-    'post_type' => 'projects',
-    'posts_per_page' => 10,
-    'paged' => $paged,
-];
-$query = new WP_Query($args);
-
-if ($query->have_posts()) :
-    while ($query->have_posts()) : $query->the_post();
-    get_template_part("components/project-card");
-    endwhile;
-
-    get_template_part('components/pagination.php');
-
-    wp_reset_postdata();
-else : ?>
-  <p>
-    <?php esc_html_e('No projects found.', 'idm250-2025'); ?>
-  </p>
-  <?php endif; ?>
-</div>
-
+        if ($query->have_posts()) :
+          while ($query->have_posts()) : $query->the_post();
+          // for each post, render this component
+          get_template_part("components/project-card");
+          endwhile;
+        get_template_part('components/pagination.php');
+        wp_reset_postdata();
+        else : ?>
+      <p><?php esc_html_e('No projects found.', 'idm250-2025'); ?></p>
+      <?php endif; ?>
+    </div>
+</main>
 <?php get_footer(); ?>
