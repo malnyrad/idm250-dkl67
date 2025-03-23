6db5b202e12b11b628bfccb502ad467b0b90458e
Daryn Lam <dkl67@drexel.edu>
2025-03-23 02:07:00 -0400

    Fix background image

--- single-projects.php
+++ single-projects.php
@@ -1,33 +1,31 @@
 <?php get_header(); ?>
-<?php if (has_post_thumbnail()) : ?>
-    <div class="project-thumbnail">
-        <?php the_post_thumbnail(); ?>
+<main class="bg-deco">
+    <div class="wrapper">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <h2 class="project-header"><?php the_title(); ?></h2>
+                <p class="tag-line"><?php echo get_the_excerpt(); ?></p>
+                <p class="project-categories">
+                    <?php echo get_the_term_list(
+                        get_the_ID(), // 204
+                        'project-categories', // taxonomy name
+                        '', // before
+                        ', ', // separator
+                        '' // after
+                    ); ?>
+                </p>
+                <hr>
+                <?php if (has_post_thumbnail()) : ?>
+                    <div class="project-thumbnail">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                <?php endif; ?>
+                <div class="project-content">
+                    <?php the_content(); ?>
+                </div>
+                <div class="project-tags">
+                    <?php the_tags("Tags: ", ", "); ?>
+                </div>
+        <?php endwhile; endif; ?>
     </div>
-<?php endif; ?>
-<main class="project-container">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article class="project">
-            <h2 class="project-title"><?php the_title(); ?></h2>
-            <p class="project-meta">Published on <?php the_date(); ?> in <?php the_category(", "); ?></p>
-            <div>
-            <p><strong>Project Categories:</strong>
-                <?php echo get_the_term_list(
-                    get_the_ID(), // 204
-                    'project-categories', // taxonomy name
-                    '', // before
-                    ', ', // separator
-                    '' // after
-                ); ?>
-            </p>
-            </div>
-            <hr>
-            <div class="project-content">
-                <?php the_content(); ?>
-            </div>
-            <div class="project-tags">
-                <?php the_tags("Tags: ", ", "); ?>
-            </div>
-        </article>
-    <?php endwhile; endif; ?>
 </main>
 <?php get_footer(); ?>
