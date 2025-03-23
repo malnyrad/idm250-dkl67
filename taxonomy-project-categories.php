82d9d7415aeca37193ef012b0f08f1920b4f33d9
Daryn Lam <dkl67@drexel.edu>
2025-03-23 03:23:08 -0400

    Update archive page

--- taxonomy-project-categories.php
+++ taxonomy-project-categories.php
@@ -3,21 +3,13 @@
   <div class="wrapper">
     <h2 class="page-header center-align"><?php single_term_title(); ?></h2>
     <?php
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        $args = [
-            'post_type' => 'projects',
-            'posts_per_page' => 10,
-            'paged' => $paged,
-        ];
-        $query = new WP_Query($args);
-
-        if ($query->have_posts()) :
-          while ($query->have_posts()) : $query->the_post();
+        if (have_posts()) :
+          while (have_posts()) : the_post();
           // for each post, render this component
           get_template_part("components/project-card");
           endwhile;
-        get_template_part('components/pagination.php');
         wp_reset_postdata();
+        the_posts_pagination();
         else : ?>
       <p><?php esc_html_e('No projects found.', 'idm250-2025'); ?></p>
       <?php endif; ?>
