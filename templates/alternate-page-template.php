eaa8f11cb3316f0c29703e077e9311f9eebc8fb3
Daryn Lam <dkl67@drexel.edu>
2025-03-23 01:16:12 -0400

    Update custom page template

--- templates/alternate-page-template.php
+++ templates/alternate-page-template.php
@@ -4,13 +4,20 @@
     */
 ?>
 <?php get_header(); ?>
-<main>
-    <h2 class="page-header center-align"><?php echo strtolower(get_the_title()); ?></h2>
-    <?php if (has_post_thumbnail()) : ?>
-    <div class="featured-image">
-        <?php the_post_thumbnail(); ?>
+<main class="bg-deco">
+    <div class="wrapper">
+        <div class="alt-hero">
+            <h2 class="alt-page-header center-align"><?php echo get_the_title(); ?></h2>
+            <?php if (has_post_thumbnail()) : ?>
+            <div class="alt-page-image">
+                <?php the_post_thumbnail(); ?>
+                <p><?php echo get_the_excerpt(); ?></p>
+            </div>
+        </div>
+        <?php endif; ?>
+        <div class="alt-main-content">
+            <?php echo get_the_content(); ?>
+        </div>
     </div>
-    <?php endif; ?>
-    <?php get_template_part("components/content"); ?>
 </main>
 <?php get_footer(); ?>
